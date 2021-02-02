ba8126dda2ef9f6e1d49f3e8ec2786ea54d19b49
Koichi874 <busiwork.k@gmail.com>
2021-02-02 20:10:45 +0900

    商品出品機能のための修正

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,18 +1,18 @@
 FactoryBot.define do
-  factory :item do
-    name             { Faker::Food.fruits }
-    description      { '商品の説明' }
-    category_id      { 1 }
-    condition_id     { 1 }
-    shipping_cost_id { 1 }
-    area_id          { 1 }
-    time_to_ship_id  { 1 }
-    price            { Faker::Number.within(range: 300..9_999_999) }
-    user_id          { 1 }
-    association :user
+  # factory :item do
+    # name             { Faker::Food.fruits }
+    # description      { '商品の説明' }
+    # category_id      { 1 }
+    # condition_id     { 1 }
+    # shipping_cost_id { 1 }
+    # area_id          { 1 }
+    # time_to_ship_id  { 1 }
+    # price            { Faker::Number.within(range: 300..9_999_999) }
+    # user_id          { 1 }
+    # association :user
 
-    after(:build) do |item|
-      item.image.attach(io: File.open('public/apple-touch-icon.png'), filename: 'apple-touch-icon.png')
-    end
-  end
+    # after(:build) do |item|
+      # item.image.attach(io: File.open('public/apple-touch-icon.png'), filename: 'apple-touch-icon.png')
+    # end
+  # end
 end
