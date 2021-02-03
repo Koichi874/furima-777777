7a280d402349899e155ff7a1020b8fd112a34b18
Koichi874 <busiwork.k@gmail.com>
2021-02-03 21:27:53 +0900

    商品購入機能のための修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :items
-  has_many :orders
+  # has_many :orders
 
   PASSWORD_REGEX = /\A(?=.*?\d)[a-z\d]+\z/i.freeze
   validates :password, presence: true, length: { minimum: 6 }, confirmation: true,
