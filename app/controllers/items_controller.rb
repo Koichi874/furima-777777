5ff1d36b478efb8be15c29ab574b2dc040357032
Koichi874 <busiwork.k@gmail.com>
2021-02-03 12:44:18 +0900

    Revert "商品詳細表示機能のための修正"
    This reverts commit 488fd5c405de47f189ec13fbdf27fce0ad9ad07a.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
-  # before_action :set_item, only: [:show, :edit, :update, :destroy]
-  # before_action :correct_user, only: [:edit, :update]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
+  before_action :correct_user, only: [:edit, :update]
 
   def index
     @items = Item.order(created_at: :desc)
@@ -20,11 +20,11 @@ class ItemsController < ApplicationController
     end
   end
 
-  # def show
-  # end
+  def show
+  end
 
-  # def edit
-  # end
+  def edit
+  end
 
   def update
     if @item.update(item_params)
@@ -43,18 +43,18 @@ class ItemsController < ApplicationController
     end
   end
 
-  # private
+  private
 
-  # def item_params
-    # params.require(:item).permit(:name, :price, :description, :category_id, :condition_id, :shipping_cost_id, :area_id,
-                                #  :time_to_ship_id, :image).merge(user_id: current_user.id)
-  # end
+  def item_params
+    params.require(:item).permit(:name, :price, :description, :category_id, :condition_id, :shipping_cost_id, :area_id,
+                                 :time_to_ship_id, :image).merge(user_id: current_user.id)
+  end
 
-  # def set_item
-    # @item = Item.find(params[:id])
-  # end
+  def set_item
+    @item = Item.find(params[:id])
+  end
 
-  # def correct_user
-    # redirect_to root_path if current_user.id != @item.user.id || @item.order.present?
-  # end
+  def correct_user
+    redirect_to root_path if current_user.id != @item.user.id || @item.order.present?
+  end
 end
